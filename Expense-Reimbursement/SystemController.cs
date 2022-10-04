cfbacfa35bc749fe666a11461942a24a976306f5
Jacob Allen <jallen1887@gmail.com>
2022-10-04 17:04:54 -0400

    Create Main Menu

---
+++ Expense-Reimbursement/SystemController.cs
@@ -0,0 +1,51 @@
+static class SystemController
+{
+    public static void StartSystem() 
+    {
+        PrintMainMenu();
+        GetMainMenuChoice();
+    }
+    private static void PrintMainMenu()
+    {
+        Console.Clear();
+        Console.WriteLine("-------------------------------------------------");
+        Console.WriteLine("              Reimbursement System               ");
+        Console.WriteLine("-------------------------------------------------");
+        Console.WriteLine("");
+        Console.WriteLine("Please choose an option below:");
+        Console.WriteLine("[1] Login existing user");
+        Console.WriteLine("[2] Register new user");
+        Console.WriteLine("[q] Quit");
+
+    }
+
+    private static void GetMainMenuChoice()
+    {
+        string? input = Console.ReadLine();
+
+        if (input == null)
+        {
+            PrintMainMenu();
+            Console.WriteLine("Please choose a valid input.");
+            GetMainMenuChoice();
+        }
+        else if (input == "1") 
+        {
+            Console.WriteLine("Login Screen");
+        }
+        else if (input == "2") 
+        {
+            Console.WriteLine("Register User");
+        }
+        else if (input == "q") 
+        {
+            Console.WriteLine("Goodbye!");
+        }
+        else
+        {
+            PrintMainMenu();
+            Console.WriteLine("Please choose a valid input.");
+            GetMainMenuChoice();
+        }
+    }
+}
